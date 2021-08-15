25634280f8a6cfe1ff61fe6c2eea79f18b225064
javier <devjavicont@gmail.com>
2021-08-15 12:11:36 -0400

    iniciar repositorio

---
+++ JS/scripts.js
@@ -0,0 +1,6 @@
+"use strict";
+const botonMenu = document.getElementById("boton-interruptor-menu");
+const menuPrincipal = document.getElementById("menu-principal");
+botonMenu.addEventListener("click", () => {
+    menuPrincipal.classList.toggle("esta-activo")
+})
