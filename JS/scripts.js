cd4eae206672dfae49867699d255aec0c641242b
javier <devjavicont@gmail.com>
2021-12-26 19:44:08 -0400

    Boton del menu: ahora tiene iconos svg

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,4 +3,5 @@ const botonMenu = document.getElementById("boton-interruptor-menu");
 const menuPrincipal = document.getElementById("menu-principal");
 botonMenu.addEventListener("click", () => {
     menuPrincipal.classList.toggle("esta-activo")
+    botonMenu.classList.toggle("esta-activo")
 })
