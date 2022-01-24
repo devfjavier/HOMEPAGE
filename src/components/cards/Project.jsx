b3618a54a717245283d1b8cd50e28c6ea246375a
javier <devjavicont@gmail.com>
2022-01-23 20:33:01 -0400

    Card Project: nuevo componente con estilos de tipo css module

---
+++ src/components/cards/Project.jsx
@@ -0,0 +1,16 @@
+import styles from "../../styles/cards/project.module.scss"
+
+const Project = ({key}) => {
+  return (
+    <article key={key} className={styles.proyecto}>
+      <div className={styles["contenedor-img"]}><img loading="lazy" src="https://raw.githubusercontent.com/devfjavier/Proyecto-de-EDsoft/master/src/images/proyecto.webp" alt=""/></div>
+      <div className={styles.contenedor}>
+        <h3 className={styles.titulo}>Proyecto</h3>
+        <p className={styles.texto}>Lorem ipsum dolor sit amet.</p>
+        <a className={styles.enlace} href="" target="_blank" rel="nofollow noreferrer noopener">Ver proyecto</a>
+      </div>
+    </article>
+  )
+}
+
+export default Project
