cdd0521fb5d3f9fb6b5f913c4db5d04ceda39f17
javier <devjavicont@gmail.com>
2022-01-23 20:41:33 -0400

    Projects: importa Card Project

--- src/components/pages/projects/Projects.jsx
+++ src/components/pages/projects/Projects.jsx
@@ -1,3 +1,5 @@
+import Project from "../../cards/Project"
+
 const Projects = () => {
   const projects = []
   for (let index = 0; index < 12; index++) {
@@ -10,16 +12,7 @@ const Projects = () => {
         <h1 className="titulo">Proyectos</h1>
         <div className="contenedor">
           {
-            projects.map(id => 
-              <article key={id} className="proyecto">
-                <div className="contenedor-img"><img loading="lazy" src="https://raw.githubusercontent.com/devfjavier/Proyecto-de-EDsoft/master/src/images/proyecto.webp" alt=""/></div>
-                <div className="contenido">
-                  <h3 className="titulo">Proyecto</h3>
-                  <p className="texto">Lorem ipsum dolor sit amet.</p>
-                  <a href="" target="_blank" rel="nofollow noreferrer noopener">Ver proyecto</a>
-                </div>
-              </article>
-            )
+            projects.map(id => <Project key={id}/>)
           }
         </div>
       </section>
