a4b898cdcdd21036195a3fc2cbd8e96cff1fd8b1
javier <devjavicont@gmail.com>
2022-03-22 19:00:07 -0400

    Card Project: agrega la ruta de la imagen

--- src/components/cards/Project.jsx
+++ src/components/cards/Project.jsx
@@ -1,9 +1,10 @@
 import styles from "../../styles/cards/project.module.scss"
+import IMG_PATH from "../../images/project.webp"
 
 const Project = () => {
   return (
     <article className={styles.proyecto}>
-      <div className={styles["contenedor-img"]}><img loading="lazy" src="https://raw.githubusercontent.com/devfjavier/Proyecto-de-EDsoft/master/src/images/proyecto.webp" alt=""/></div>
+      <div className={styles["contenedor-img"]}><img loading="lazy" src={IMG_PATH} alt=""/></div>
       <div className={styles.contenedor}>
         <h3 className={styles.titulo}>Proyecto</h3>
         <p className={styles.texto}>Lorem ipsum dolor sit amet.</p>
