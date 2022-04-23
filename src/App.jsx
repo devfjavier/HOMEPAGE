3aff3ec2497fdb3342f0edfd985b137aeb503119
javier <devjavicont@gmail.com>
2022-04-23 15:15:09 -0400

    Agrega code-splitting a las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import "./styles/styles.scss"
+import { lazy, Suspense } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Header from "./components/core-sections/Header"
 import Footer from "./components/core-sections/Footer"
-import Page404 from "./components/core-sections/Page404"
-import Home from "./components/pages/home/Home"
-import WhoWeAre from "./components/pages/who-we-are/WhoWeAre"
-import Projects from "./components/pages/projects/Projects"
-import Contact from "./components/pages/contact/Contact"
+import Home from"./components/pages/home/Home"
+const WhoWeAre = lazy(() => import("./components/pages/who-we-are/WhoWeAre"))
+const Projects = lazy(() => import("./components/pages/projects/Projects"))
+const Contact = lazy(() => import("./components/pages/contact/Contact"))
+const Page404 = lazy(() => import("./components/core-sections/Page404"))
 
 const App = () => (
   <BrowserRouter basename="/HOMEPAGE">
     <Header/>
-
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about-us" element={<WhoWeAre/>}/>
-      <Route path="/projects" element={<Projects/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="*" element={<Page404/>}/>
-    </Routes>
-
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about-us" element={<WhoWeAre/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="*" element={<Page404/>}/>
+      </Routes>
+    </Suspense>
     <Footer/>
   </BrowserRouter>
 )
